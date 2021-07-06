40ffd914d9a77c284ab96d02aa2db70b62f18c57
Empty13-13 <maks.ept1@gmail.com>
2021-07-06 23:05:28 +0500

    vue project now

---
+++ precache-manifest.cbbb67fe8e4668d6e8b46031f68e9be6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab79a94f60870587051",
+    "url": "/css/app.bb4ad4a1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7aad08e98f65dcfb5f5b19a4d652b652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "891d9fe5b4405c9a2708",
+    "url": "/js/about.6cabb685.js"
+  },
+  {
+    "revision": "5ab79a94f60870587051",
+    "url": "/js/app.d9d494fe.js"
+  },
+  {
+    "revision": "73573be58b9caa06ed34",
+    "url": "/js/chunk-vendors.3d365fc9.js"
+  },
+  {
+    "revision": "7c1d92a79bafb9bee6a2c09c96a14b5f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
